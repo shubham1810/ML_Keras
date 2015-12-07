5c876c379e6e1cbbc5e9c6e4cc625fb07b1db211
Shubham Dokania <skd.1810@gmail.com>
2015-12-07 13:07:51 +0530

    Add method to test sampling data and TTS engine

---
+++ test_and_load.py
@@ -0,0 +1,103 @@
+from __future__ import print_function
+from keras.models import Sequential, model_from_json
+from keras.layers.core import Dense, Activation, Dropout
+from keras.layers.recurrent import LSTM
+from keras.datasets.data_utils import get_file
+import numpy as np
+import random
+import sys
+import os
+
+
+def load_if_available():
+    try:
+        print("Trying to load model data")
+        model = model_from_json(open("model_file.json").read())
+        model.load_weights("my_model_weights.h5")
+        return model
+    except:
+        print("Failed to load weights...")
+        return None
+
+
+print('Build model...')
+
+model = load_if_available()
+print('Model compiled successfully....waiting to start')
+
+
+path = get_file('nietzsche.txt', origin="https://s3.amazonaws.com/text-datasets/nietzsche.txt")
+text = open(path).read().lower()
+print('corpus length:', len(text))
+
+chars = set(text)
+print('total chars:', len(chars))
+char_indices = dict((c, i) for i, c in enumerate(chars))
+indices_char = dict((i, c) for i, c in enumerate(chars))
+
+# cut the text in semi-redundant sequences of maxlen characters
+maxlen = 20
+step = 3
+sentences = []
+next_chars = []
+for i in range(0, len(text) - maxlen, step):
+    sentences.append(text[i: i + maxlen])
+    next_chars.append(text[i + maxlen])
+print('nb sequences:', len(sentences))
+
+print('Vectorization...')
+X = np.zeros((len(sentences), maxlen, len(chars)), dtype=np.bool)
+y = np.zeros((len(sentences), len(chars)), dtype=np.bool)
+for i, sentence in enumerate(sentences):
+    for t, char in enumerate(sentence):
+        X[i, t, char_indices[char]] = 1
+    y[i, char_indices[next_chars[i]]] = 1
+
+# build the model: 2 stacked LSTM
+def sample(a, temperature=1.0):
+    # helper function to sample an index from a probability array
+    a = np.log(a) / temperature
+    a = np.exp(a) / np.sum(np.exp(a))
+    return np.argmax(np.random.multinomial(1, a, 1))
+
+
+def run_main():
+    start_index = random.randint(0, len(text) - maxlen - 1)
+    print('Enter a string:')
+    inp = raw_input()
+
+    if len(inp) < maxlen:
+        val = inp + text[start_index: start_index + maxlen - len(inp)]
+        inp = val
+    if len(inp) > maxlen:
+        inp = inp[:maxlen]
+
+    print('You entered: ' + inp)
+
+    for diversity in [0.2, 0.5, 1.0, 1.2]:
+        speak_sentence = ''
+        print()
+        print('----- diversity:', diversity)
+        generated = ''
+        #sentence = text[start_index: start_index + maxlen]
+        sentence = inp
+        generated += sentence
+        print('----- Generating with seed: "' + sentence + '"')
+        sys.stdout.write(generated)
+        for iteration in range(400):
+            x = np.zeros((1, maxlen, len(chars)))
+            for t, char in enumerate(sentence):
+                x[0, t, char_indices[char]] = 1.
+            preds = model.predict(x, verbose=0)[0]
+            next_index = sample(preds, diversity)
+            next_char = indices_char[next_index]
+            generated += next_char
+            sentence = sentence[1:] + next_char
+            sys.stdout.write(next_char)
+            speak_sentence += next_char
+            sys.stdout.flush()
+        print()
+        os.system('say '+speak_sentence)
+
+
+
